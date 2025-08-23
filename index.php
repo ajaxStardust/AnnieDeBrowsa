90904aececde018ea1fd571c09ed089e0ada2ac8
ajaxStardust <ajaxStardust@gmail.com>
2025-08-22 21:08:55 -0400

    jfedora okay on this branch

--- index.php
+++ index.php
@@ -10,9 +10,12 @@ $uri = urldecode(
 
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 require_once __DIR__.'/public/index.php';
+    
+    
 }
 else {
     header('Location: public/index.php');
+    
 }
 
  
