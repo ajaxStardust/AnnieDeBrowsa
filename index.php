f72ed129a1854d60b252f94f94b31ec1a143af29
novicenotes <novicenotes@gmail.com>
2025-11-07 14:17:27 -0500

    couple of edits in vscode win 11

--- index.php
+++ index.php
@@ -9,11 +9,12 @@ $uri = urldecode(
 );
 
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-require_once __DIR__.'/public/index.php';
-    
+    header('Location: public/index.php');
+require_once __DIR__.'/public/index.php';   
     
 }
 else {
+    require_once __DIR__.'/public/index.php';
     header('Location: public/index.php');
     
 }
