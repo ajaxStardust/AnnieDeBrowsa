fbf3c264274837273d2bd1bca17168a3ac88b688
ajaxstardust <ajaxstardust@gmail.com>
2025-12-28 15:18:31 -0500

    jori.jori

--- i_am_become_url.sh
+++ i_am_become_url.sh
@@ -53,7 +53,7 @@ if [ $# -eq 0 ]; then
         sleep 3
 
         # Check if HOSTNAME is set in the SYSTEM ENV and valid. Uncomment next line to define HOSTNAME here instead. tweak with MYHostname
-        HOSTNAME='adb.statecollegeguitarlessons.site'
+        HOSTNAME='transformative.click'
         # MYHOSTNAME='centrewebdesign.lan' #OR WHATEVER you want it to be here is where you can define
         # ADBPATH='anniedebrowsa' #eg the developer setup default
 
@@ -65,26 +65,29 @@ if [ $# -eq 0 ]; then
                 HOSTNAME=$MYHOSTNAME
 
         HOSTNAME=${HOSTNAME}"/"${ADBPATH}
-                    printf "%s\n" "${RED}No HOSTNAME set, using default: ${HOSTNAME}${RESET}"
+        printf "%s\n" 
+                    printf "%s\n" "${RED}Sending to MYHOSTNAME: ${YELLOW}${HOSTNAME}${RESET}"
                     else
-                        printf "%s\n" "${CYAN}Using system HOSTNAME: ${HOSTNAME}${RESET}"
+                    printf "%s\n" 
+                        printf "%s\n" "${CYAN}Sending to HOSTNAME:${GREEN}${HOSTNAME}${RESET}"
         fi
-
+sleep 3
                         # Construct the URL and ensure it is properly formatted
-                        URL="http://${HOSTNAME}/public/index.phtml?path2url=${FULL_PATH}"
+                        URL="http://${HOSTNAME}/public/default.php?path2url=${FULL_PATH}"
 
                         # Open browser with the constructed URL
 #                        printf "%s\n" "${YELLOW}Open browser (using xdg-open alias):${RESET}"
 #                        printf "%s\n" "${CYAN}The PHP app AnnieDeBrowsa should be at path eg. /var/www/html/anniedebrowsa${RESET}"
-#                        sleep 1
+                       
 #                        printf "%s\n" "${YELLOW}Confirm your environment is correct at line 40 in transform.sh${RESET}"
-#                        sleep 3
+#                     sleep 3
 
                         # Use xdg-open to open the URL in the default browser
-                        printf "%s\n" "${BLUE}Click the URL now or wait for the explanation. %s\n  $URL {$RESET}"
+     
 
                         # Show the variables used
 #                        sleep 5
+printf "%s\n" 
                         printf "%s\n" "${CYAN}Variables used include:${RESET}"
                         printf "%s\n"
                         printf "%s\n" "${YELLOW}PWD:${RESET} ${GREEN}$PWD${RESET}"
