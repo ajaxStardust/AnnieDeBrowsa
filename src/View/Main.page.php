9752eed7c5f465efabbb1d909e1c1c36ea6882a5
ajaxstardust <ajaxstardust@gmail.com>
2026-01-01 17:05:18 -0500

    details to Main.page

--- src/View/Main.page.php
+++ src/View/Main.page.php
@@ -210,15 +210,15 @@ $resultsWithDescriptions = [
     <input type="text" v-model="selectedUrl" placeholder="Type or select a URL..." class="w-100 pa2 mt2 ba b--gray br2">
 
     <div class="mt2">
-      <p>Powered by <strong>Vue3 v-bind</strong>. Edit the URL if necessary.</p>
-      <p>Choose a radio-button from the available results to place that URL here.</p>
+      <p>Powered by <span class="bg-lightest-blue">Vue3 v-bind</span>. Edit the URL if necessary.</p>
+      <p><strong class="bg-yellow">Choose a radio-button</strong> from the available results to place that URL here.</p>
       <p><strong><a v-bind:href="selectedUrl">{{ selectedUrl }}</a></strong></p>
       <div class="mt2">
         <details>
             <summary>Details</summary>
             <p>Edit <code class="bg-near-white br2">./src/View/Main.page.php</code> to customize this <mark>View</mark>. E.g. MVC</p>
             <p>Modify the logic in the PHP <code class="bg-near-white br2">./src/Model/P2u2.php</code> to <mark>Model</mark> the URL based on your specific development environment.</p>
-            <p>Experiment with <mark>Vue.js</mark> to dynamically transform the resulting URL, based on events or other conditions.</p>
+            <p>Experiment with <mark class="bg-lightest-blue">Vue.js</mark> to dynamically transform the resulting URL, based on events or other conditions.</p>
         </details>
       </div>
     </div>
