7d6ef8f5f9ae760043be1ad5a4ed344ff193931c
ajaxstardust <ajaxstardust@gmail.com>
2026-01-01 16:50:52 -0500

    details to Main.page

--- src/View/Main.page.php
+++ src/View/Main.page.php
@@ -213,6 +213,14 @@ $resultsWithDescriptions = [
       <p>Powered by <strong>Vue3 v-bind</strong>. Edit the URL if necessary.</p>
       <p>Choose a radio-button from the available results to place that URL here.</p>
       <p><strong><a v-bind:href="selectedUrl">{{ selectedUrl }}</a></strong></p>
+      <div class="mt2">
+        <details>
+            <summary>Details</summary>
+            <p>Edit <code class="bg-near-white br2">./src/View/Main.page.php</code> to customize this <mark>View</mark>. E.g. MVC</p>
+            <p>Modify the logic in the PHP <code class="bg-near-white br2">./src/Model/P2u2.php</code> to <mark>Model</mark> the URL based on your specific development environment.</p>
+            <p>Experiment with <mark>Vue.js</mark> to dynamically transform the resulting URL, based on events or other conditions.</p>
+        </details>
+      </div>
     </div>
 
 
