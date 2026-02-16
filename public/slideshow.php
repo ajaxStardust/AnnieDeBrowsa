d206204564590b53ecdf860e70dc81a2e07afa51
Jeffrey Sabarese <ajaxstardust@duck.com>
2026-02-16 17:37:25 -0500

    repaired kensington

--- public/slideshow.php
+++ public/slideshow.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace P2u2;
-<<<<<<< HEAD
  
 error_reporting(E_ALL);
 $page_heading = 'LAYOUT (DOCTYPE) UNICODE - u1f390-u1f5ff';
@@ -12,19 +11,13 @@ require NS2_ROOT . '/vendor/autoload.php';
 require NS2_ROOT . '/public/doctype/doctype-base.php';
 
 ?>
+
 <body>
-<div id="pagewidth" class="container">
+    <div id="pagewidth" class="container">
 
-  <section id="slideshow">  <!-- #HEADER -->
-      <h1 class="bg-dark text-light text-end pe-3"><?php print $page_heading; ?></h1>
+        <section id="slideshow">
+            <!-- #HEADER -->
+            <h1 class="bg-dark text-light text-end pe-3"><?php print $page_heading; ?></h1>
 
-<p>The images will appear here once the correct function is created.</p>
-  </section>
-  
-=======
-error_reporting(0);
-define('NS2', __NAMESPACE__);
-define('NS2_ROOT', dirname(__DIR__));
-require NS2_ROOT . '/vendor/autoload.php';
-require  NS2_ROOT . '/src/View/Slideshow.page.php';
->>>>>>> efbbf295 (transformative.jori)
+            <p>The images will appear here once the correct function is created.</p>
+        </section>
---
+++ template.picnic-css.php
@@ -0,0 +1,4 @@
+<?php
+header('Location: ./public/template.picnic.php');
+exit();
+?>
